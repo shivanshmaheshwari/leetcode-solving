353ee8d5f02e735c710812ca802f282fdb4126e9
Shivansh Maheshwari <78491674+shivanshmaheshwari@users.noreply.github.com>
2024-09-20 00:05:40 +0530

    Time: 35 ms (75.18%), Space: 16.8 MB (27.58%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
@@ -1,18 +1,29 @@
 class Solution:
     def diffWaysToCompute(self, input: str) -> List[int]:
-            if input.isdigit():
-                return [int(input)]
+        # Memoization dictionary to store the results of subproblems
+        memo = {}
+        
+        def compute(expression):
+            if expression.isdigit():
+                return [int(expression)]
+            if expression in memo:
+                return memo[expression]
+            
             res = []
-            for i in range(len(input)):
-                if input[i] in "-+*":
-                    left = self.diffWaysToCompute(input[:i])
-                    right = self.diffWaysToCompute(input[i+1:])
+            for i in range(len(expression)):
+                if expression[i] in "-+*":
+                    left = compute(expression[:i])
+                    right = compute(expression[i+1:])
                     for l in left:
                         for r in right:
-                            if input[i] == '+':
-                                res.append(l+r)
-                            elif input[i] == '-':
-                                res.append(l-r)
-                            elif input[i] == '*':
-                                res.append(l*r)
+                            if expression[i] == '+':
+                                res.append(l + r)
+                            elif expression[i] == '-':
+                                res.append(l - r)
+                            elif expression[i] == '*':
+                                res.append(l * r)
+            
+            memo[expression] = res
             return res
+        
+        return compute(input)
